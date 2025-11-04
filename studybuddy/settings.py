d3cd521eed1be4df53dfa9419a711c2919004a13
princederek <marteyderek08@gmail.com>
2025-11-04 16:11:45 +0000

    add db

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'studybuddy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER':'postgres',
-        'PASSWORD':'jYnzUxrrCuVIUNwejJNCGYveJOVxGSsO',
-        'HOST':'turntable.proxy.rlwy.net',
-        'PORT':'50140',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER':'postgres',
+#         'PASSWORD':'jYnzUxrrCuVIUNwejJNCGYveJOVxGSsO',
+#         'HOST':'turntable.proxy.rlwy.net',
+#         'PORT':'50140',
+#     }
+# }
 
 
 # Password validation
